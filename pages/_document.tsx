67786506fd27362351634b72d8da0f7100facf1d
yesitsmeagainhi <bwtnaresh@gmail.com>
2025-07-03 18:27:07 +0530

    Add Netlify Identity widget to handle invite tokens

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,30 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Netlify Identity Widget - handles invite tokens on any page */}
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
+        
+        {/* Auto-redirect to /admin after login */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if (window.netlifyIdentity) {
+                window.netlifyIdentity.on('init', user => {
+                  if (!user) {
+                    window.netlifyIdentity.on('login', () => {
+                      document.location.href = '/admin/';
+                    });
+                  }
+                });
+              }
+            `,
+          }}
+        />
       </body>
     </Html>
   );
